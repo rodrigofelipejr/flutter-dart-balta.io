da7bf99b8c059fb3cb6d8444189c3322f4b092fd
Rodrigo Felipe <rodrigo.felipe@inovamobil.com.br>
2020-11-04 18:01:37 -0300

    Dominando Layouts no Flutter - Animated Icon

--- app_shopping/lib/widgets/search-box.widget.dart
+++ app_shopping/lib/widgets/search-box.widget.dart
@@ -8,6 +8,7 @@ class SearchBox extends StatefulWidget {
 class _SearchBoxState extends State<SearchBox>
     with SingleTickerProviderStateMixin {
   AnimationController _animationController;
+  bool menuOpened = false;
 
   /* 
     LEMBRE-SE DE INSTANCIAR O CONTROLLER E FAZER O DISPOSE, PARA QUE NÃO OCORRA 
@@ -33,42 +34,24 @@ class _SearchBoxState extends State<SearchBox>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //color: Colors.black.withOpacity(0.1),
-      height: 60,
-      padding: EdgeInsets.only(left: 20),
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(0, 0, 0, 0.1),
-        borderRadius: BorderRadius.all(
-          Radius.circular(128),
-        ),
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.search),
-          Container(
-            width: 250,
-            padding: EdgeInsets.only(left: 10),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.go,
-              decoration: InputDecoration(
-                labelText: "Search...",
-                border: InputBorder.none,
-                labelStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 16,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-          )
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("Search"),
+        FlatButton(
+          onPressed: () {
+            !menuOpened
+                ? _animationController.forward() // play animation
+                : _animationController.reverse(); // reverse animation
+            menuOpened = !menuOpened;
+          },
+          child: AnimatedIcon(
+            icon: AnimatedIcons.menu_close,
+            progress: _animationController,
+            semanticLabel: 'Open Menu',
+          ),
+        )
+      ],
     );
   }
 }

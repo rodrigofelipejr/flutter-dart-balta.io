c79f33caa85a3bcf4e9976ca7c139a35e5d19aed
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-08 20:02:37 -0300

    Flutter Layouts - Contact App - Criando o formulário

--- app_contact/contacts/lib/android/views/editor-contact.view.dart
+++ app_contact/contacts/lib/android/views/editor-contact.view.dart
@@ -1,3 +1,4 @@
+import 'package:contacts/android/styles.dart';
 import 'package:contacts/models/contact.model.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,44 @@ class EditorContactView extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextFormField(
+              initialValue: model?.name,
+              onSaved: (val) {
+                model.name = val;
+              },
+            ),
+            TextFormField(
+              initialValue: model?.phone,
+              onSaved: (val) {
+                model.phone = val;
+              },
+            ),
+            TextFormField(
+              initialValue: model?.email,
+              onSaved: (val) {
+                model.email = val;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FlatButton(
+              onPressed: () {},
+              color: primaryColor,
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                  color: accentColor,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

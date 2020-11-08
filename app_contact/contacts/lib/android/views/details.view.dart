b87b8fd06e8997be79c7a387c27cb3c17fe9c21a
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-08 18:59:42 -0300

    Flutter Layouts - Contact App - Ações do contato

--- app_contact/contacts/lib/android/views/details.view.dart
+++ app_contact/contacts/lib/android/views/details.view.dart
@@ -1,4 +1,7 @@
+import 'package:contacts/android/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class DetailsView extends StatelessWidget {
   @override
@@ -9,6 +12,99 @@ class DetailsView extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: primaryColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://source.unsplash.com/user/erondu/200x200"),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "André Figueiredo",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "11 9 8845-3278",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Text(
+            "andrefig@ig.com",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FlatButton(
+                onPressed: () {},
+                color: primaryColor,
+                shape: CircleBorder(
+                  side: BorderSide.none,
+                ),
+                child: Icon(
+                  Icons.phone,
+                  color: accentColor,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: primaryColor,
+                shape: CircleBorder(
+                  side: BorderSide.none,
+                ),
+                child: Icon(
+                  Icons.email,
+                  color: accentColor,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: primaryColor,
+                shape: CircleBorder(
+                  side: BorderSide.none,
+                ),
+                child: Icon(
+                  Icons.camera_enhance,
+                  color: accentColor,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

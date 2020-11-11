8279c8d2ab98a05ca2a5ed3f854d0676cda3e214
Rodrigo Felipe <rodrigo.felipe@inovamobil.com.br>
2020-11-11 13:57:18 -0300

    Perfect Eggs - Theme

--- app_eggs/lib/main.dart
+++ app_eggs/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_eggs/themes/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      title: 'Perfect Eggs',
+      theme: appTheme(),
       home: Container(),
     );
   }

880efbce8cd15bec9b2f9411972fa7b95158ebc2
Rodrigo Felipe <rodrigo.felipe@inovamobil.com.br>
2020-11-12 14:29:39 -0300

    Todo App: Mobile com Flutter, MobX e Firebase - Component button     - Criando o componente do Botão     - Utilizando o Botão

--- app_todo_2/lib/main.dart
+++ app_todo_2/lib/main.dart
@@ -1,18 +1,34 @@
+import 'package:app_todo_2/components/button.widget.dart';
 import 'package:app_todo_2/themes/app.theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Todos',
       theme: appTheme(),
-      home: Container(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: cButton(
+          text: "Teste",
+          callback: () {},
+          width: 300,
+          image: "assets/images/google.png",
+        ),
+      ),
     );
   }
 }

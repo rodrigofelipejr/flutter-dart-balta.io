073fb69114fb80d290a0c5c5b02245cf965ec81e
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-12 06:47:16 -0300

    Perfect Eggs - Configurando o Provider

--- app_eggs/lib/main.dart
+++ app_eggs/lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:app_eggs/pages/splash.page.dart';
 import 'package:app_eggs/themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'blocs/app.bloc.dart';
+
+void main() => (MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppBloc>.value(
+          value: AppBloc(),
+        ),
+      ],
+      child: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {

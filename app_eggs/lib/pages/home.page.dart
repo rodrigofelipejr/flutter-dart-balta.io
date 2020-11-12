6a68c79b35cb4828abc7d33e2f66fb879ecda9b7
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-12 07:21:32 -0300

    Perfect Eggs - Controlando as Ações

--- app_eggs/lib/pages/home.page.dart
+++ app_eggs/lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:app_eggs/blocs/type-buttons.widget.dart';
+import 'package:app_eggs/widgets/action-buttons.widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,9 +10,29 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 60,
+              height: 50,
             ),
-            TypeButtons(),
+            Text(
+              "Perfect Eggs",
+              style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            //Eggs(),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TypeButtons(),
+            ),
+            //Progress(),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ActionButtons(),
+            ),
           ],
         ),
       ),

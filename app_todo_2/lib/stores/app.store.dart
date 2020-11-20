aa197e005ad1e1467f21c42ab90c53edd8678891
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-20 06:52:51 -0300

    Todo App: Mobile com Flutter, MobX e Firebase - Gerando o código   - flutter packages pub run build_runner build

--- app_todo_2/lib/stores/app.store.dart
+++ app_todo_2/lib/stores/app.store.dart
@@ -3,10 +3,12 @@ import 'package:mobx/mobx.dart';
 part 'app.store.g.dart'; /* esta classe esta dividida em duas pastes */
 
 // app.store.g.dart (g => generator)
-
+// assa aqui vamos distribuir, (_$AppStore vem do app.store.g.dart -> automaticamente (prefixo _$) + nomme da classe)
 class AppStore = _AppStore with _$AppStore;
 
-class _AppStore with Store {
+// with -> mixin?
+// _ pq ela vai ser privada, não será distrbuida e sim a junção dela
+abstract class _AppStore with Store {
   @observable // anotação
   String currentState = "none";
 
---
+++ app_todo_2/lib/stores/app.store.g.dart
@@ -0,0 +1,111 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app.store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$AppStore on _AppStore, Store {
+  final _$currentStateAtom = Atom(name: '_AppStore.currentState');
+
+  @override
+  String get currentState {
+    _$currentStateAtom.reportRead();
+    return super.currentState;
+  }
+
+  @override
+  set currentState(String value) {
+    _$currentStateAtom.reportWrite(value, super.currentState, () {
+      super.currentState = value;
+    });
+  }
+
+  final _$busyAtom = Atom(name: '_AppStore.busy');
+
+  @override
+  bool get busy {
+    _$busyAtom.reportRead();
+    return super.busy;
+  }
+
+  @override
+  set busy(bool value) {
+    _$busyAtom.reportWrite(value, super.busy, () {
+      super.busy = value;
+    });
+  }
+
+  final _$todosAtom = Atom(name: '_AppStore.todos');
+
+  @override
+  ObservableList<TodoItem> get todos {
+    _$todosAtom.reportRead();
+    return super.todos;
+  }
+
+  @override
+  set todos(ObservableList<TodoItem> value) {
+    _$todosAtom.reportWrite(value, super.todos, () {
+      super.todos = value;
+    });
+  }
+
+  final _$_AppStoreActionController = ActionController(name: '_AppStore');
+
+  @override
+  void changeSelected(String state) {
+    final _$actionInfo = _$_AppStoreActionController.startAction(
+        name: '_AppStore.changeSelected');
+    try {
+      return super.changeSelected(state);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void add(TodoItem item) {
+    final _$actionInfo =
+        _$_AppStoreActionController.startAction(name: '_AppStore.add');
+    try {
+      return super.add(item);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setTodos(ObservableList<TodoItem> items) {
+    final _$actionInfo =
+        _$_AppStoreActionController.startAction(name: '_AppStore.setTodos');
+    try {
+      return super.setTodos(items);
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void clearTodos() {
+    final _$actionInfo =
+        _$_AppStoreActionController.startAction(name: '_AppStore.clearTodos');
+    try {
+      return super.clearTodos();
+    } finally {
+      _$_AppStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+currentState: ${currentState},
+busy: ${busy},
+todos: ${todos}
+    ''';
+  }
+}

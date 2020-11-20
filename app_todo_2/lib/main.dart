98a519072779addf52c1cfc7269bfe56a579594b
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-20 07:13:23 -0300

    Todo App: Mobile com Flutter, MobX e Firebase - Provider   - Provider e Singleton Store

--- app_todo_2/lib/main.dart
+++ app_todo_2/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:app_todo_2/stores/app.store.dart';
 import 'package:app_todo_2/themes/app.theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'views/login.view.dart';
 
@@ -16,11 +18,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Todos',
-      theme: appTheme(),
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        Provider<AppStore>.value(
+          value: AppStore(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Todos',
+        theme: appTheme(),
+        home: LoginView(),
+      ),
     );
   }
 }

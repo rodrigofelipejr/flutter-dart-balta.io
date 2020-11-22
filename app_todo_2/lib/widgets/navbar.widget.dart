aeaf2c9d218ace8553426192e004262c068f071c
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-22 10:42:15 -0300

    Todo App: Mobile com Flutter, MobX e Firebase   - Refatorando o componente Navbar

--- app_todo_2/lib/widgets/navbar.widget.dart
+++ app_todo_2/lib/widgets/navbar.widget.dart
@@ -1,41 +1,60 @@
+import 'package:app_todo_2/controllers/todo.controller.dart';
+import 'package:app_todo_2/stores/app.store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<AppStore>(context);
+    final controller = new TodoController(store);
+
     return Container(
       width: double.infinity,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          FlatButton(
-            onPressed: () {},
-            child: Text(
-              "Hoje",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          Observer(
+            builder: (_) => FlatButton(
+              onPressed: () {
+                controller.changeSelection("today");
+              },
+              child: Text(
+                "Hoje",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == "today" ? FontWeight.bold : FontWeight.w100,
+                ),
               ),
             ),
           ),
-          FlatButton(
-            onPressed: () {},
-            child: Text(
-              "Amanhã",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.normal,
+          Observer(
+            builder: (_) => FlatButton(
+              onPressed: () {
+                controller.changeSelection("tomorrow");
+              },
+              child: Text(
+                "Amanhã",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == "tomorrow" ? FontWeight.bold : FontWeight.w100,
+                ),
               ),
             ),
           ),
-          FlatButton(
-            onPressed: () {},
-            child: Text(
-              "Todas",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.normal,
+          Observer(
+            builder: (_) => FlatButton(
+              onPressed: () {
+                controller.changeSelection("all");
+              },
+              child: Text(
+                "Todas",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == "all" ? FontWeight.bold : FontWeight.w100,
+                ),
               ),
             ),
           ),

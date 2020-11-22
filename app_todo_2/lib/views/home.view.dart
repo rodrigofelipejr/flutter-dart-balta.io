11d4b3dc94f03f250c30c4bafb93cd989bb23ed9
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-22 10:47:08 -0300

    Todo App: Mobile com Flutter, MobX e Firebase - Configurando o Startup da Aplicação

--- app_todo_2/lib/views/home.view.dart
+++ app_todo_2/lib/views/home.view.dart
@@ -1,11 +1,22 @@
+import 'package:app_todo_2/controllers/todo.controller.dart';
+import 'package:app_todo_2/stores/app.store.dart';
 import 'package:app_todo_2/widgets/navbar.widget.dart';
 import 'package:app_todo_2/widgets/todo-list.widget.dart';
 import 'package:app_todo_2/widgets/user-card.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<AppStore>(context);
+    final controller = new TodoController(store);
+
+    // Só ocorre na primeira vez que o app rodar
+    if (store.currentState == "none") {
+      controller.changeSelection("today");
+    }
+
     return Scaffold(
       body: Column(
         children: [

fdb51c1e6f97418f52cc3346fd50c407791fe446
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-22 10:16:15 -0300

    Todo App: Mobile com Flutter, MobX e Firebase - Configurando HTTPS

--- app_todo_2/lib/main.dart
+++ app_todo_2/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app_todo_2/stores/app.store.dart';
 import 'package:app_todo_2/themes/app.theme.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,11 +8,20 @@ import 'package:provider/provider.dart';
 
 import 'views/login.view.dart';
 
-// void main() => runApp(MyApp());
+// Esta classe permite acesso ao LocalHost com certificados HTTPS inválidos
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    HttpClient client = super.createHttpClient(context);
+    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    return client;
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
 

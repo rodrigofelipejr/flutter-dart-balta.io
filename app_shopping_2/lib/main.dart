ba175597459066e689273707d47e0b9c3650ee09
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-29 16:39:06 -0300

    Flutter, BloC e Provider - HomePage

--- app_shopping_2/lib/main.dart
+++ app_shopping_2/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app_shopping_2/blocs/home.bloc.dart';
-import 'package:app_shopping_2/ui/shared/widgets/category/category-list.widget.dart';
-import 'package:app_shopping_2/ui/shared/widgets/product/product-list.widget.dart';
+import 'package:app_shopping_2/ui/android/pages/home.page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
@@ -32,51 +31,6 @@ class Main extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // pegando uma versão que já está na memória
-    final HomeBloc bloc = Provider.of<HomeBloc>(context);
-
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              "Categorias",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CategoryList(
-              categories: bloc.categories,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Main Vendidos",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ProductList(
-              products: bloc.products,
-            )
-          ],
-        ),
-      ),
     );
   }
 }

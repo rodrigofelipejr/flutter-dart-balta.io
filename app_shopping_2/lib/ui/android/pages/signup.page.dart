83c5323c5a498654667b759d57972fa41ebe7343
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-12-01 20:16:33 -0300

    Flutter, BloC e Provider - Signup Page

--- app_shopping_2/lib/ui/android/pages/signup.page.dart
+++ app_shopping_2/lib/ui/android/pages/signup.page.dart
@@ -1,8 +1,168 @@
+import 'package:app_shopping_2/blocs/account.bloc.dart';
+import 'package:app_shopping_2/models/create-user.model.dart';
+import 'package:app_shopping_2/ui/shared/validators/cursom.validators.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  var user = new CreateUserModel();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "Nome",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Nome inválido";
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  user.name = val;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) => CustomValidators.isEmail(value),
+                onSaved: (val) {
+                  user.email = val;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "Usuário",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Usuário inválido";
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  user.name = val;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Senha inválida";
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  user.password = val;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    _create(context);
+                  }
+                },
+                child: Text("Cadastrar"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _create(BuildContext context) async {
+    var bloc = Provider.of<AccountBloc>(context, listen: false);
+    var _user = await bloc.create(user);
+
+    if (_user == null) {
+      final snackbar = SnackBar(content: Text("Não foi possível realizar seu cadastro!"));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+    } else {
+      final snackbar = SnackBar(content: Text("Bem-vindo! Autentique-se"));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+      Navigator.pop(context);
+    }
   }
 }

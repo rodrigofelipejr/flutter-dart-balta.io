dbbde190f14705f1bf22b7f2b3517d2adeed755f
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-12-01 06:55:37 -0300

    Flutter, BloC e Provider - Account Repository

---
+++ app_shopping_2/lib/repositories/account.repository.dart
@@ -0,0 +1,20 @@
+import 'package:app_shopping_2/models/create-user.model.dart';
+import 'package:dio/dio.dart';
+import 'package:app_shopping_2/models/authenticate-user.model.dart';
+import 'package:app_shopping_2/models/user.model.dart';
+
+import '../settings.dart';
+
+class AccountRepository {
+  Future<UserModel> authenticate(AuthenticateModel model) async {
+    var url = "${Settings.apiUrl}v1/account/login";
+    Response response = await Dio().post(url, data: model);
+    return UserModel.fromJson(response.data);
+  }
+
+  Future<UserModel> create(CreateUserModel model) async {
+    var url = "${Settings.apiUrl}v1/account";
+    Response response = await Dio().post(url, data: model);
+    return UserModel.fromJson(response.data);
+  }
+}
